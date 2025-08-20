a0db5f3bd49c99f2e9f01a8567e17b8f11468ab5
Sitecom244 <jeroen@specishops.nl>
2025-08-20 10:16:15 +0200

    Update fefco 201 calculator.js

--- fefco 201 calculator.js
+++ fefco 201 calculator.js
@@ -374,10 +374,10 @@ document.getElementById('submit-request').addEventListener('click', function(eve
     document.getElementById('submit-request').style.display = 'none';
     document.getElementById('loading-indicator').style.display = 'block';
 
-    // Geef de browser even de tijd om .value updates toe te passen
-    setTimeout(() => {
+    // Wacht tot de DOM echt geüpdatet is vóór het verzenden van het formulier
+    requestAnimationFrame(() => {
         document.querySelector('form').submit();
-    }, 50);
+    });
 });
 
 
@@ -385,3 +385,4 @@ document.getElementById('submit-request').addEventListener('click', function(eve
     calcformCalculate();
     updateWarningAndButton();
 });
+
